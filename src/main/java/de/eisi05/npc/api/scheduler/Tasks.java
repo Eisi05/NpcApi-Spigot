9ba2cc3ed0941de9a7f53b9f20ad5198177ddbd7
Eisi05 <clechi05@gmail.com>
2026-08-31 22:22:40 +0200

    Fixed bug when using without PlaceholderAPI

--- src/main/java/de/eisi05/npc/api/scheduler/Tasks.java
+++ src/main/java/de/eisi05/npc/api/scheduler/Tasks.java
@@ -141,6 +141,9 @@ public class Tasks
      */
     public static void updateSkin(@NotNull Player player, @NotNull NPC npc, @NotNull NpcSkin npcSkin)
     {
+        if(!Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI"))
+            return;
+
         String newPlaceholder = (String) Reflections.invokeStaticMethod("me.clip.placeholderapi.PlaceholderAPI", "setPlaceholders", player,
                 npcSkin.getPlaceholder()).get();
 
